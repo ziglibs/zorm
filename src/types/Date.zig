aefbdffbbc3ee19b85c55ff888add887385a6dbe
i0bs <41456914+i0bs@users.noreply.github.com>
2023-02-04 14:39:15 -0500

    feat: finish relative date logic

--- src/types/Date.zig
+++ src/types/Date.zig
@@ -1,50 +1,77 @@
 const std = @import("std");
 const time = std.time;
+const mem = std.mem;
 
-/// A date structure based off of a proleptic Gregorian calendar.
-///
-/// There are three valid ways to represent a date:
-///
-/// - Absolute: denotes the year, month and day.
-/// - Relative: based off of another date, with a difference between the current system date
-///             and the one given.
-/// - Span: the difference between two given dates, whether absolute or relative.
-///
-/// The objectivity of this structure is to allow all three ways to be valid use cases.
-/// Dates will always require a year, month and day for a relative/span representation.
-/// Failing to do so will result in an improper date.
 pub const Date = @This();
 
-// Possible errors for a Date
-const Error = error {
-    MissingYear,
-    MissingMonth,
-    MissingDay,
-    InvalidSpan
-};
+const Error = error { Missing };
 
-/// The year of the date.
-year: i64,
+year: ?i64 = undefined,
+month: ?u8 = undefined,
+week: ?u8 = undefined,
+day: ?u8 = undefined,
 
-/// The month of the date.
-month: u8,
-
-/// The day of the date.
-day: u8,
-
-/// Creates a new Date.
-pub fn init(year: i64, month: u8, day: u8) Date {
+pub fn new(year: i64, month: u8, day: u8) Date {
     return Date {
         .year = year,
         .month = month,
+        .week = day % 7,
         .day = day
     };
 }
 
-/// Retrieves the current system date.
-pub fn today() !Date {
-    const ts = try time.Instant.now();
+fn validate(self: *Date, date: ?Date) !bool {
+    // A date can still be created by struct alone - we have to run some checks.
+    if (self.year == undefined or date.year.? == undefined) return Error.Missing;
+    if (self.month == undefined or date.month.? == undefined) return Error.Missing;
+    if (self.day == undefined or date.day.? == undefined) return Error.Missing;
+}
 
-    // TODO: process the Instant timestamp data and place it into a Date structure.
-    return Date { .year = undefined, .month = undefined, .day = undefined };
+pub fn now() !Date {
+    // An instant will be an unsigned 64-bit integer for a POSIX value in milliseconds.
+    const current_time = try time.Instant.now();
+
+    const ms_per_month: u8 = 30 * time.ms_per_day;
+    const ms_per_year: i64 = 12 * ms_per_month;
+
+    return Date.new(
+        current_time / ms_per_year,
+        current_time / ms_per_month,
+        current_time / time.ms_per_day
+    );
+}
+
+pub fn from(self: *Date, date: ?Date) !?Date {
+    if (
+        self.year.? == date.year.? and
+        self.month.? == date.month.? and
+        self.day.? == date.day.?
+    )
+        return null;
+
+    try self.validate();
+
+    if (date.?) {
+        const current_date = try Date.now();
+
+        return Date.new(
+            self.year - current_date.year,
+            self.month - current_date.month,
+            self.day - current_date.day
+        );
+    }
+
+    else return Date.new(
+        self.year - date.year,
+        self.month - date.month,
+        self.day - date.day
+    );
+}
+
+pub fn toString(
+    self: *Date,
+    format: enum { @"ISO 8601", @"MM-DD-YYYY" }
+) error { Unsupported }![]const u8 {
+    // TODO: play with std.fmt() fuckery
+    return error.Unsupported;
 }
