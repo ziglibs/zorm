d0ab7f94629d04597656d5b84b17501b3933a07c
i0bs <41456914+i0bs@users.noreply.github.com>
2023-02-07 15:12:30 -0500

    chore: clean date debug code
    Closes #1

--- src/main.zig
+++ src/main.zig
@@ -7,8 +7,4 @@ const UUID = @import("./types/UUID.zig");
 
 pub fn main() !void {
     std.debug.print("{s}\n\n", .{"Hello, world!"});
-
-    // testing date code
-    const date = try Date.fromString("2023-07-23", .@"YYYY-MM-DD");
-    std.debug.print("{?}", .{date.year});
 }
