4202fa3b50afd7cdaa2f6112059830c837b48904
i0bs <41456914+i0bs@users.noreply.github.com>
2023-03-17 00:03:28 -0400

    fix: make datatypes public accessible

--- src/main.zig
+++ src/main.zig
@@ -1,14 +1,12 @@
 const std = @import("std");
 const testing = std.testing;
 
-const Self = @This();
+pub const Date = @import("types/Date.zig").Date;
+pub const Field = @import("types/Field.zig").Field;
+pub const Object = @import("types/Object.zig").Object;
+pub const UUID = @import("types/UUID.zig").UUID;
 
-const Date = @import("./types/Date.zig");
-const Field = @import("./types/Field.zig").Field;
-const Object = @import("./types/Object.zig").Object;
-const UUID = @import("./types/UUID.zig");
-
-const create = @import("./gen.zig").create;
+pub const create = @import("gen.zig").create;
 
 pub fn main() !void {
     std.debug.print("{s}\n\n", .{"Hello, world!"});
